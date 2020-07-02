e1e43a2c4c75f0c1cab3cc54dd890c1c834a1579
Myles Carey <seanmyles@comcast.net>
2020-07-01 18:09:10 -0700

    Started work on Letter class

--- src/Letter.java
+++ src/Letter.java
@@ -1,6 +1,15 @@
 public class Letter {
+    Character letter;
+    Boolean isKnown;
+
+    public Letter(char letter) {
+        this.letter = letter;
+        this.isKnown = (letter == ' ') ? true : false;
+    }
+
     // placeholder method
     public void summary() {
         System.out.println("in Letter class object summary method");
+        System.out.println("Letter is: " + letter + " is known is: " + isKnown);
     }
 }
--- src/Word.java
+++ src/Word.java
@@ -2,7 +2,9 @@ public class Word {
     // placeholder method
     public void summary() {
         System.out.println("in Word class object summary method");
-        Letter letter = new Letter();
+        Letter letter = new Letter('Q');
         letter.summary();
+        Letter letter2 = new Letter(' ');
+        letter2.summary();
     }
 }
