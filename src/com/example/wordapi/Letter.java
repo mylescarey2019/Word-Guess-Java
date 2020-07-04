1cab31999ba2c8360a13182c4871be34ddacc393
Myles Carey <seanmyles@comcast.net>
2020-07-03 17:08:09 -0700

    adding getter, setter and methods to Letter class.  Switched properties to private

--- src/com/example/wordapi/Letter.java
+++ src/com/example/wordapi/Letter.java
@@ -1,17 +1,42 @@
 package com.example.wordapi;
 
 public class Letter {
-    Character letter;
-    Boolean isKnown;
+    private char _letter;
+    private boolean _isKnown;
 
     public Letter(char letter) {
-        this.letter = letter;
-        this.isKnown = (letter == ' ') ? true : false;
+        this._letter = letter;
+        this._isKnown = letter == ' ';
+    }
+
+    // getter - if letter is not known display an underscore
+    public char getLetter() {
+        return _isKnown ? _letter : '-';
+    }
+
+    // temporary helper method
+    public char peakLetter() {
+        return _letter;
+    }
+
+    // a method acting as a 'setter' for _isKnown property
+    public void guessLetter(char letter) {
+        _isKnown = Character.toUpperCase(letter) == Character.toUpperCase(_letter) || getIsKnown();
+    }
+
+    public boolean getIsKnown() {
+        return _isKnown;
+    }
+
+    // force the letter to known state without guessing
+    public void forceReveal() {
+        _isKnown = true;
     }
 
     // placeholder method
     public void summary() {
         System.out.println("in com.example.wordapi.Letter class object summary method");
-        System.out.println("in com.example.wordapi.Letter is: " + letter + " is known is: " + isKnown);
+        System.out.println("in com.example.wordapi.Letter is: " + getLetter() + " is known is: " + getIsKnown() +
+                " revealed value is: " + peakLetter());
     }
 }
